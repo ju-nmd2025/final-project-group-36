421aed75f5509734de6326f00e720d39143a0afa
Adeliina <adeliina.lampela@gmail.com>
2025-12-03 12:55:52 +0100

    add moving character feature

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ function setup() {
 
 let canvasWidth = 400;
 let canvasHeight = 400;
-let character = new Character(50, 50, 50, 50);
+let character = new Character(175, 350, 50, 50);
 
 function draw() {
   background(100, 100, 100);
@@ -31,13 +31,13 @@ function draw() {
   } else if (movingPlatform.x > 250) {
     movingPlatform.dir = 5;
   }
-}
 
-if (
-  character.y + character.h < 300 &&
-  !character.isColliding(character, platform)
-) {
-  character.y += 10;
+  if (
+    character.y + character.h < 300 &&
+    !character.isColliding(character, platform)
+  ) {
+    character.y += 10;
+  }
 }
 
 /*floor code becomes platform function keyPressed() {
@@ -45,3 +45,16 @@ if (
         character.y -= 120;
     }
 }*/
+
+function moveCharacter(e) {
+  if (e.keyCode == 37) {
+    character.x -= 10;
+  }
+  if (e.keyCode == 39) {
+    character.x += 10;
+  }
+  if (e.keyCode == 32) {
+    character.y -= 100;
+  }
+}
+document.onkeydown = moveCharacter;
