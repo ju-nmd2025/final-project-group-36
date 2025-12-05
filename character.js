d0f8e47411716eef577c375788491e1a45670ea6
CLaura155 <chikan.laura@gmail.com>
2025-12-05 16:21:52 +0100

    Bug fixed: character doesn't float after moving right from the platform

--- character.js
+++ character.js
@@ -1,22 +1,25 @@
 export default class Character {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    //		this.isOnPlatForm = false;
+  }
 
-    	constructor(x, y, w, h) {
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-//		this.isOnPlatForm = false;
-	}
+  draw() {
+    rect(this.x, this.y, this.w, this.h);
+  }
 
-    draw() {
-        rect(this.x, this.y, this.w, this.h);
-    }
-
-    isColliding(character, platform) {
-    if (platform.y === character.y + character.w && platform.x <= character.x + character.w) {
-        return true;
+  isColliding(character, platform) {
+    if (
+      platform.y === character.y + character.w &&
+      platform.x <= character.x + character.w &&
+      platform.x + platform.w >= character.x
+    ) {
+      return true;
     } else {
-        return false;
+      return false;
     }
-}
+  }
 }
