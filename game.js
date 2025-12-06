5b31e5a015d9271cd2f82c97d597dee8a58cb469
Adeliina <adeliina.lampela@gmail.com>
2025-12-06 17:39:30 +0100

    add platforms into array

--- game.js
+++ game.js
@@ -16,18 +16,36 @@ let character = new Character(
   50,
   50
 );
+let platforms = [];
+
 let platform = new Platform(
   Math.random() * (canvasWidth - 80),
   canvasHeight - 100
 );
-let platform2 = new Platform(Math.random() * (canvasWidth - 80), 225);
-
+let platform2 = new Platform(
+  Math.random() * (canvasWidth - 80),
+  canvasHeight - 200
+);
+let platform3 = new Platform(
+  Math.random() * (canvasWidth - 80),
+  canvasHeight - 300
+);
+let platform4 = new Platform(
+  Math.random() * (canvasWidth - 80),
+  canvasHeight - 400
+);
+let platform5 = new Platform(
+  Math.random() * (canvasWidth - 80),
+  canvasHeight - 500
+);
+platforms.push(platform, platform2, platform3, platform4, platform5);
 function draw() {
   background(100, 100, 100);
 
   character.draw();
-  platform.draw();
-  platform2.draw();
+  for (let thePlatform of platforms) {
+    thePlatform.draw();
+  }
 
   // still platform
   /* platform.x = 10;
